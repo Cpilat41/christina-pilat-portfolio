3b8a0b30ed32ad21642d2445fd2586dbe8253a5a
Christina pilat <cpmoneyy@gmail.com>
2022-08-26 17:35:13 -0400

    change css link

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import AboutMe from "./Components/AboutMe";
 import Contact from "./Components/Contact";
 import Resume from "./Components/Resume";
 // import Header from "./Components/Header";
-import "/christina-pilat-portfolio/styles/App.css";
+import "christina-pilat-portfolio/styles/App.css";
 
 function App() {
   return (
